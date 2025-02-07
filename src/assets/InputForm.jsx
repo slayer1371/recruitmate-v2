502decc0a2a9e17f576ab7ff70696a2658edfadf
Mrinal Sharma <mrinalwork2@gmail.com>
2025-02-07 09:52:11 -0500

    iphone-glitch

--- src/assets/InputForm.jsx
+++ src/assets/InputForm.jsx
@@ -59,6 +59,7 @@ const InputForm = () => {
         <section className="bg-gray-50 dark:bg-gray-900">
           <div className="flex flex-row items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 ">
             <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 ">
+              <Fade cascade>
               <div className="p-6 space-y-4 md:space-y-6 sm:p-8 ">
                 <a className="flex items-center mb-6 text-xl font-semibold dark:text-white">
                   <img className=" h-8 mr-2" src={rmf} alt="logo" />
@@ -138,8 +139,29 @@ const InputForm = () => {
                       <option value="football">Football</option>
                     </select>
                   </div>
+                  <div>
+                    <label
+                      htmlFor="sport"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      How did you hear about us?
+                    </label>
+                    <select
+                      name="advert"
+                      id="advert"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      required
+                    >
+                      <option value="">Select an Option</option>
+                      <option value="instagram">Instagram</option>
+                      <option value="twitter">X(Twitter)</option>
+                      <option value="tiktok">TikTok</option>
+                      <option value="online">Referral(Friends, Coach, Teammate)</option>
+                      <option value="referred">Web Search</option>
+                    </select>
+                  </div>
 
-                  <div className="flex items-start">
+                  {/* <div className="flex items-start">
                     <div className="flex items-center h-5">
                       <input
                         id="terms"
@@ -163,7 +185,7 @@ const InputForm = () => {
                         </a>
                       </label>
                     </div>
-                  </div>
+                  </div> */}
                   <button
                     type="submit"
                     className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -172,6 +194,7 @@ const InputForm = () => {
                   </button>
                 </form>
               </div>
+              </Fade>
             </div>
             <div>
               <img src={signupimg} />
